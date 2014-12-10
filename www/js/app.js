09d0219f4a544f2ff552b6591e00ae6688ee03ee
chakradhar <debianmaster@chakradhars-mbp.att.net>
2014-12-10 01:32:55 -0800

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,127 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers','ngCordova'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    //console.log(window.cordova);
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "templates/menu.html",
+    controller: 'AppCtrl'
+  })
+
+  .state('app.search', {
+    url: "/search",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/search.html"
+      }
+    }
+  })
+
+  .state('app.browse', {
+    url: "/browse",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/browse.html"
+      }
+    }
+  })
+    .state('app.playlists', {
+      url: "/playlists",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/playlists.html",
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: "/playlists/:playlistId",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/playlist.html",
+        controller: 'PlaylistCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/playlists');
+});
+
+
+angular.module.factory('MediaSrv', function($q, $ionicPlatform, $window){
+  var service = {
+    loadMedia: loadMedia,
+    getStatusMessage: getStatusMessage,
+    getErrorMessage: getErrorMessage
+  };
+
+  function loadMedia(src, onError, onStatus, onStop){
+    var defer = $q.defer();
+    $ionicPlatform.ready(function(){
+      var mediaSuccess = function(){
+        if(onStop){onStop();}
+      };
+      var mediaError = function(err){
+        _logError(src, err);
+        if(onError){onError(err);}
+      };
+      var mediaStatus = function(status){
+        if(onStatus){onStatus(status);}
+      };
+
+      if($ionicPlatform.is('android')){src = '/android_asset/www/' + src;}
+      defer.resolve(new $window.Media(src, mediaSuccess, mediaError, mediaStatus));
+    });
+    return defer.promise;
+  }
+
+  function _logError(src, err){
+    console.error('media error', {
+      code: err.code,
+      message: getErrorMessage(err.code)
+    });
+  }
+
+  function getStatusMessage(status){
+    if(status === 0){return 'Media.MEDIA_NONE';}
+    else if(status === 1){return 'Media.MEDIA_STARTING';}
+    else if(status === 2){return 'Media.MEDIA_RUNNING';}
+    else if(status === 3){return 'Media.MEDIA_PAUSED';}
+    else if(status === 4){return 'Media.MEDIA_STOPPED';}
+    else {return 'Unknown status <'+status+'>';}
+  }
+
+  function getErrorMessage(code){
+    if(code === 1){return 'MediaError.MEDIA_ERR_ABORTED';}
+    else if(code === 2){return 'MediaError.MEDIA_ERR_NETWORK';}
+    else if(code === 3){return 'MediaError.MEDIA_ERR_DECODE';}
+    else if(code === 4){return 'MediaError.MEDIA_ERR_NONE_SUPPORTED';}
+    else {return 'Unknown code <'+code+'>';}
+  }
+
+  return service;
+})
